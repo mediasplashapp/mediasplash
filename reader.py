8ccaac2ab3b2824c4e01bee1beee90c822017f47
mohamedSulaimanAlmarzooqi <uae566340007@hotmail.com>
2022-05-16 18:27:07 +0400

    Now uses uuid for files in tempdir instead of actual file titles for avoiding possible errors.

--- reader.py
+++ reader.py
@@ -2,9 +2,13 @@ import json
 import subprocess
 from cleaner import clean
 import os
+import uuid
+import logging
+
 
 def generate_subtitles(filename, temp_dir):
     info = subprocess.getoutput(f'ffprobe -v error  -show_entries stream -print_format json "{filename}"')
+    logging.debug(info)
     data = {}
     try:
         data = json.loads(info)
@@ -16,7 +20,9 @@ def generate_subtitles(filename, temp_dir):
     for i in data['streams']:
         if "codec_name" in i and "index" in i and i['codec_type'] == 'subtitle':
             subtitle_name = f"{i['tags']['title']}.ass"
-            subprocess.call(["ffmpeg", "-i", filename, "-map", f"0:{i['index']}", f"{os.path.join(temp_dir.name, subtitle_name)}"], shell = True)
-            clean(os.path.join(temp_dir.name, subtitle_name))
-            final[subtitle_name] = (i['disposition']['default'], f"{os.path.join(temp_dir.name, subtitle_name)}")
+            subtitle_file = f"{uuid.uuid4().hex}.ass"
+            res = subprocess.getoutput(["ffmpeg", "-i", filename, "-map", f"0:{i['index']}", f"{os.path.join(temp_dir.name, subtitle_file)}"])
+            logging.debug(res)
+            clean(os.path.join(temp_dir.name, subtitle_file))
+            final[subtitle_name.replace(".ass", "")] = (i['disposition']['default'], f"{os.path.join(temp_dir.name, subtitle_file)}")
     return final
